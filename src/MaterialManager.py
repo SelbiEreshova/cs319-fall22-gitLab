19c8e5659d6b822fe2857aa00fafbfd7792c9cad
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2021-10-20 20:17:16 +0300

    refactor

--- src/MaterialManager.py
+++ src/MaterialManager.py
@@ -8,21 +8,33 @@ class MaterialManager:
         # other configurations
 
     def changeMaterial(self, material) -> bool:
-        returnText = ""
-
-        match material:
-            case materialType.Titanium:
-                self.carManager.setMaterial(materialType.Titanium)
-                returnText = "Material is changed to Titanium."
-
-            case materialType.Carbon:
-                self.carManager.setMaterial(materialType.Carbon)
-                returnText = "Material is changed to Carbon."
-
-            case _:
-                returnText = "Material seletion is invalid."
-
-        return self._announceMaterial(returnText)
+            returnText = ""
+            textBaseForValidMaterial = "Material is changed to"
+    
+            match material:
+                case materialType.Titanium:
+                    self.carManager.setMaterial(materialType.Titanium)
+                    returnText = textBaseForValidMaterial + " Titanium."
+    
+                case materialType.Carbon:
+                    self.carManager.setMaterial(materialType.Carbon)
+                    returnText = textBaseForValidMaterial + " Carbon."
+    
+                case materialType.AlloyX:
+                    self.carManager.setMaterial(materialType.AlloyX)
+                    returnText = textBaseForValidMaterial + " AlloyX."
+    
+                case _:
+                    returnText = "Material selection is invalid."
+    
+            return self._announceMaterial(returnText)
 
     def _announceMaterial(self, text: str) -> bool:
-        return self.announcer(text)
+       returnValue = False
+       try:
+           returnValue = self.announcer(text)
+       except:
+           print("Announcement of material type is failed.")
+           returnValue = False
+       finally:
+           return returnValue
